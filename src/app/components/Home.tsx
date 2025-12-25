62199e704fb7bb09e62b39a8115a9b8f16483675
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-12-25 15:49:05 +0530

    okay

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { Variants } from "framer-motion";
 import Lady from "../../../public/Images/lady.png"
-import Ban from "../../../public//images/ban.png"
+import Ban from "../../../public/images/ban.png"
 export default function HeroSection() {
   // Animation variants
   const containerVariants: Variants  = {
